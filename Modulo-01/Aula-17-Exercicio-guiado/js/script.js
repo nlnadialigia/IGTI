7cda1a12083130a90407ec56c1fac5a7f4825404
Nadia Ligia <nlnadialigia@gmail.com>
2020-11-23 12:44:51 -0300

    add renderFavorites and renderSummary

--- Modulo-01/Aula-17-Exercicio-guiado/js/script.js
+++ Modulo-01/Aula-17-Exercicio-guiado/js/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 let tabCountries = null;
 let tabFavorites = null;
 
@@ -12,25 +13,25 @@ let countFavorites = 0;
 
 let numberFormat = null;
 
-window.addEventListener("load", () => {
-  tabCountries = document.querySelector("#tabCountries");
-  tabFavorites = document.querySelector("#tabFavorites");
-  countContries = document.querySelector("#countCountries");
-  countFavorites = document.querySelector("#countFavorites");
-  totalPopulationList = document.querySelector("#totalPopulationList");
-  totalPopulationFavorites = document.querySelector(
-    "#totalPopulationFavorites"
+window.addEventListener('load', () => {
+  tabCountries = document.getElementById('tabCountries');
+  tabFavorites = document.getElementById('tabFavorites');
+  countContries = document.getElementById('countCountries');
+  countFavorites = document.getElementById('countFavorites');
+  totalPopulationList = document.getElementById('totalPopulationList');
+  totalPopulationFavorites = document.getElementById(
+    'totalPopulationFavorites',
   );
 
-  numberFormat = Intl.NumberFormat("pt-BR");
+  numberFormat = Intl.NumberFormat('pt-BR');
 
   fetchCountries();
 });
 
 async function fetchCountries() {
-  const response = await fetch("https://restcountries.eu/rest/v2/all");
+  const response = await fetch('https://restcountries.eu/rest/v2/all');
   const data = await response.json();
-  allCountries = data.map((country) => {
+  allCountries = data.map(country => {
     const { numericCode, translations, population, flag } = country;
     return {
       id: numericCode,
@@ -39,6 +40,8 @@ async function fetchCountries() {
       flag,
     };
   });
+
+  allFavorites = allCountries;
 
   render();
 }
@@ -52,16 +55,18 @@ function render() {
 
 function renderCountryList() {
   console.log(allCountries);
-  let countriesHtml = "";
-  allCountries.forEach((country) => {
-    const { name, flag, population } = country;
+  let countriesHtml = '';
+  allCountries.forEach(country => {
+    const { id, name, flag, population } = country;
     const countryHtml = `
       <div class="country">
-        <div class="add">+</div>
+        <div class="waves-effect waves=light btn" id=${id}>+</div>
         <img src=${flag} alt="Bandeira ${name}">
         <div class="data">
-          <p>${name}</p>
-          <p>${population}</p>
+          <ul>
+            <li>${name}</li>
+            <li>${population}</li>
+          </ul> 
         </div>
       </div>
     `;
@@ -73,13 +78,48 @@ function renderCountryList() {
 }
 
 function renderFavorites() {
-  console.log(renderFavorites);
+  let favoritesHtml = '';
+  allFavorites.forEach(country => {
+    const { id, name, flag, population } = country;
+    const favoriteCountryHtml = `
+      <div class="country">
+        <div class="waves-effect waves=light btn red darken-4" id=${id}>-</div>
+        <img src=${flag} alt="Bandeira ${name}" />
+        <div class="data">
+          <ul>
+            <li>${name}</li>
+            <li>${population}</li>
+          </ul>
+        </div>
+      </div>
+    `;
+
+    favoritesHtml += favoriteCountryHtml;
+  });
+
+  tabFavorites.innerHTML = favoritesHtml;
 }
 
 function renderSummary() {
-  console.log(renderSummary);
+  countContries.textContent = allCountries.length;
+  countFavorites.textContent = allFavorites.length;
+
+  const totalPopulation = allCountries.reduce((accumulator, country) => {
+    return accumulator + country.population;
+  }, 0);
+
+  const totalFavorites = allFavorites.reduce((accumulator, country) => {
+    return accumulator + country.population;
+  }, 0);
+
+  totalPopulationList.textContent = formatNumber(totalPopulation);
+  totalPopulationFavorites.textContent = formatNumber(totalFavorites);
 }
 
 function handleCountryButtons() {
-  console.log("handleCountryButtons");
+  console.log('handleCountryButtons');
+}
+
+function formatNumber(number) {
+  return numberFormat.format(number);
 }
