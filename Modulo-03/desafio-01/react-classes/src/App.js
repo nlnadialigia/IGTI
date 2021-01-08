845ae036d3609f10da8b8bbce77a0c07df41fb8d
Nadia Ligia <nlnadialigia@gmail.com>
2021-01-08 00:10:32 -0300

    Challenge 01 - React-class

--- Modulo-03/desafio-01/react-classes/src/App.js
+++ Modulo-03/desafio-01/react-classes/src/App.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
-import ProjetoBase from './components/ProjetoBase/ProjetoBase';
+import { getNewTimestamp } from './helpers/dateTimeHelpers';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      clickArray: [],
+    };
+  }
+
+  handleClick = () => {
+    const newClickArray = Object.assign([], this.state.clickArray);
+    newClickArray.push(getNewTimestamp());
+    this.setState({ clickArray: newClickArray });
+  };
+
+  componentDiUpdate(){
+    document.title = this.state.clickArray.length.toString();
+  }
+
   render() {
-    return <ProjetoBase />;
+    const { clickArray } = this.state;
+    return (
+      <div>
+        <h1>React e Class Components</h1>
+        <button onClick={this.handleClick}>Clique aqui</button>
+        <ul>
+          {clickArray.map((item) => {
+            return <li key={item}>{item}</li>;
+          })}
+        </ul>
+      </div>
+    );
   }
 }
